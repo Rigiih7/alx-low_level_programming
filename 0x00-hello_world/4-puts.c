060fb6b26a5753c71a201cf4c521829e179c5e33
Rigiih7 <mwirigihbrian@gmail.com>
2023-02-16 09:08:07 -0800

    forgot parethenses. Compliying with Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /*include standard input output header*/
-int main(void)	/*main functions*/
+int main(void)	
+/*main function is the entry point of every code*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
