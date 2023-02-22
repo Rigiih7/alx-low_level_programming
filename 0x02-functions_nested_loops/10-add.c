af429c9e4df0703b63935317f424ac7aa7e6a013
Rigiih7 <mwirigihbrian@gmail.com>
2023-02-22 05:33:17 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the result
+ * @lavie: first number input
+ * @brian: second number input
+ * Return: lavie + brian
+ */
+
+int add(int lavie, int brian)
+{
+	return (lavie + brian);
+}
