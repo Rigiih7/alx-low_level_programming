842c53e9a03abef7487567b4c4f7a1ab55681300
Rigiih7 <mwirigihbrian@gmail.com>
2023-02-23 05:32:15 -0800

    multiplacation

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * mul - mutiplies two intengers
+ * @a: one of the intengers to be multiplied
+ * @b: one of the intengers being multiplied
+ * Return: result of multiplication of the parameters
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
