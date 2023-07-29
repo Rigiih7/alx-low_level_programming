61ae12d09921a38c4dbe53d305ef26df81ae9769
Rigiih7 <mwirigihbrian@gmail.com>
2023-07-29 04:56:06 -0700

    a program that copies the content of a file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
-* main - program that copies the content of a file to another file
-* @argc: number of argument
-* @argv: string to size to be read
-* Return: 0
+* main - a program to copy file content to another file
+* @argc: the argument count
+* @argv: pointer array to each argument
+* Return: Always 0, Success
 */
 
 int main(int argc, char *argv[])
 {
 int file_from, file_to;
-int num0 = 1024, num1 = 0;
-char buff[1024];
+int num1 = 1024, num2 = 0;
+char buf[1024];
 
 if (argc != 3)
 	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
@@ -30,16 +30,16 @@ if (file_to == -1)
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 	close(file_from), exit(99);
 }
-while (num0 == 1024)
+while (num1 == 1024)
 {
-	num0 = read(file_from, buff, 1024);
-	if (num0 == -1)
+	num1 = read(file_from, buf, 1024);
+	if (num1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	num0 = write(file_to, buff, num0);
-	if (num1 < num0)
+	num2 = write(file_to, buf, num1);
+	if (num2 < num1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 }
 
@@ -51,4 +51,3 @@ if (close(file_to) == -1)
 
 return (0);
 }
-
