4fff965fdb268ee206e3dd90a3b550a4b64e9ea1
Rigiih7 <mwirigihbrian@gmail.com>
2023-08-17 06:33:48 -0700

    print lists

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the nodes
+ * @h: head node
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
