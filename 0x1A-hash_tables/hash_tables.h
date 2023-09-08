c22bb66969d66964900b571b170214bc436d1473
Rigiih7 <mwirigihbrian@gmail.com>
2023-09-08 12:43:00 -0700

    task 3 & 4

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -36,5 +36,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+hash_node_t *set_pair(const char *key, const char *value)
+int set_pair_only(hash_table_t *ht, const char *key,
+		const char *value, unsigned long int index
 
 #endif
